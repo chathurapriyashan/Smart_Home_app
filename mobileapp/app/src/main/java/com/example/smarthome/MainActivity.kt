0b76f18f803e1b6405cfccf8b940f1913b753e7e
Chathura  Priyashan <chathurapriyashan@JPChathuras-MacBook-Air.local>
2026-07-16 18:36:43 +0530

    ready to build mobile application

---
+++ mobileapp/app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.smarthome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.smarthome.ui.theme.SmartHomeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SmartHomeTheme {
+                Surface(modifier = Modifier.safeDrawingPadding()) {
+                    Text("Hello, world!")
+                }
+
+            }
+        }
+    }
+}
